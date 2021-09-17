5c1600eb034c59c77473f457ab66df34bfeae851
dxt99 <ericlouis19x@gmail.com>
2021-09-17 16:11:13 +0800

    added basic operations and made display static

--- src/matrix.java
+++ src/matrix.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class matrix{
 	double[][] mat= new double[1000][1000];
 	int n; //matrix is n*m
-	int m;
+	int m; //m should be n+1 (spek tubes)
 	Scanner in= new Scanner(System.in);
 	
 	//constructor
@@ -15,6 +16,7 @@ public class matrix{
 	public void read(){
 		System.out.printf("Masukan Matriks:\n1. Keyboard\n2. File\nPilihan:");
 		int choice=this.in.nextInt();
+		in.nextLine(); //eats newline
 		if(choice==1)this.readKey();
 		else if(choice==2)this.readFile(); //temporary
 		else{
@@ -28,17 +30,15 @@ public class matrix{
 		this.n=this.in.nextInt();
 		in.nextLine(); //eats newline
 		System.out.printf("Masukkan banyak kolom: ");
-		this.m=this.in.nextInt();
+		this.m=this.in.nextInt(); //m should be n+1
 		in.nextLine(); //same
 		System.out.printf("Masukkan matriks:\n");
 		for(int i=0;i<this.n;i++)for(int j=0;j<this.m;j++){
 			this.mat[i][j]=this.in.nextFloat();
 			in.nextLine(); //same
 		}
-		//this.display();	
 	}
 	
-	//not done
 	void readFile(){
 		System.out.printf("Masukkan nama atau path file:\n");
 		String file=this.in.nextLine();
@@ -46,13 +46,62 @@ public class matrix{
 			Scanner filein = new Scanner(new FileReader(file));
 			int i=0;
 			while(filein.hasNextLine()){
-				// i have no idea how to read this, what the fuck!!!!
 				String s=filein.nextLine();
+				String[] temp=s.split(" ",0);
+				for(int j=0;j<temp.length;j++)this.mat[i][j]=Double.parseDouble(temp[j]);
+				i++;
 			}
+			this.n=i;
+			this.m=i+1;
 			filein.close();
-			this.display();
 		}catch(FileNotFoundException ex){
 			System.out.printf("File not found\n"); //loops back to drvier
 		}
 	}
+	
+	//testing only
+	public static void display(){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				System.out.printf("%f ",this.mat[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	//*OPERATIONS*//
+	//swaps i=baris1 and i=baris2 in this.mat[i][]
+	void tukarBaris(int baris1, int baris2){ //using 0 based index
+		double temp;
+		for(int i=0;i<this.m;i++){
+			temp=this.mat[baris1][i];
+			this.mat[baris1][i]=this.mat[baris2][i];
+			this.mat[baris2][i]=temp;
+		}
+	}
+	
+	//swaps j=kolom1 and j=kolom2 in this.[][j], for cramer
+	void tukarKolom(int kolom1, int kolom2){
+		double temp;
+		for(int i=0;i<this.n;i++){
+			temp=this.mat[i][kolom1];
+			this.mat[i][kolom1]=this.mat[i][kolom2];
+			this.mat[i][kolom2]=temp;
+		}
+	}
+	
+	//mat[baris][]*=x
+	void kaliX(int baris, double x){
+		for(int i=0;i<this.m;i++){
+			this.mat[baris][i]*=x;
+		}
+	}
+	
+	//baris1+=baris2*x
+	void tambahBaris(int baris1,int baris2, double x){
+		for(int i=0;i<this.m;i++){
+			this.mat[baris1][i]+=this.mat[baris2][i]*x;
+		}
+	}
+	
 }
